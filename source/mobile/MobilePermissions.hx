e39c1e201de7617318c11149b165e64ab1826f55
ChanceXML <joseguest1337@gmail.com>
2026-03-10 17:04:33 -0700

    Update MobilePermissions.hx

--- source/mobile/MobilePermissions.hx
+++ source/mobile/MobilePermissions.hx
@@ -1,38 +1,64 @@
-package mobile;
+package mobile.funkin.backend.utils;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+#end
 
 #if android
-import extension.androidtools.os.Build;
-import extension.androidtools.os.Build.VERSION_CODES;
-import extension.androidtools.Permissions;
-import extension.androidtools.os.Environment;
-import extension.androidtools.Settings;
-
-class MobilePermissions
-{
-    public static function request():Void
-    {
-        var perms:Array<String>;
-
-        if (Build.SDK_INT >= VERSION_CODES.TIRAMISU) {
-            perms = [
-                "android.permission.READ_MEDIA_IMAGES",
-                "android.permission.READ_MEDIA_VIDEO",
-                "android.permission.READ_MEDIA_AUDIO"
-            ];
-        } else {
-            perms = [
-                "android.permission.READ_EXTERNAL_STORAGE",
-                "android.permission.WRITE_EXTERNAL_STORAGE"
-            ];
-        }
-
-        Permissions.requestPermissions(perms);
-
-        if (Build.SDK_INT >= VERSION_CODES.R) {
-            if (!Environment.isExternalStorageManager()) {
-                Settings.requestSetting("android.settings.MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
-            }
-        }
-    }
-}
+import extension.androidtools.os.Build.VERSION as AndroidVersion;
+import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
+import extension.androidtools.Permissions as AndroidPermissions;
+import extension.androidtools.os.Environment as AndroidEnvironment;
+import extension.androidtools.Settings as AndroidSettings;
+import lime.system.System;
 #end
+
+class StorageUtil
+{
+	#if sys
+	public static function getStorageDirectory():String
+		return #if android haxe.io.Path.addTrailingSlash(AndroidContext.getExternalFilesDir()) #elseif ios lime.system.System.documentsDirectory #else Sys.getCwd() #end;
+	#end
+
+	#if android
+	public static function getExternalStorageDirectory():String
+		return "/storage/emulated/0/.CodenameEngine/";
+
+	public static function getModsPath():String
+	{
+		final externalFile:String = System.applicationStorageDirectory + "external.txt";
+		final externalStatus:String = FileSystem.exists(externalFile) ? File.getContent(externalFile) : "false";
+		return externalStatus == "true" ? StorageUtil.getExternalStorageDirectory() : StorageUtil.getStorageDirectory();
+	}
+
+	public static function requestPermissions():Void
+	{
+		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
+			AndroidPermissions.requestPermissions([
+				"READ_MEDIA_IMAGES",
+				"READ_MEDIA_VIDEO",
+				"READ_MEDIA_AUDIO",
+				"READ_MEDIA_VISUAL_USER_SELECTED"
+			]);
+		else
+			AndroidPermissions.requestPermissions([
+				"READ_EXTERNAL_STORAGE",
+				"WRITE_EXTERNAL_STORAGE"
+			]);
+
+		if (!AndroidEnvironment.isExternalStorageManager())
+			AndroidSettings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+
+		try
+		{
+			if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
+				FileSystem.createDirectory(StorageUtil.getStorageDirectory());
+		}
+		catch (e:Dynamic)
+		{
+			lime.system.System.exit(1);
+		}
+	}
+	#end
+}
