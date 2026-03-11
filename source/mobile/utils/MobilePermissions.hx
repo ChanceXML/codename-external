641af811fbed3053454a96872f08c61fcd8e7ab7
ChanceXML <joseguest1337@gmail.com>
2026-03-11 16:25:07 -0700

    Update MobilePermissions.hx

--- source/mobile/utils/MobilePermissions.hx
+++ source/mobile/utils/MobilePermissions.hx
@@ -2,29 +2,40 @@ package mobile.utils;
 
 import extension.androidtools.Settings;
 import extension.androidtools.os.Environment;
-import lime.system.System;
+import extension.androidtools.os.Permissions;
+import extension.androidtools.os.Build;
 
 class MobilePermissions
 {
     public static function request():Void
     {
         #if android
-        if (!Environment.isExternalStorageManager())
-        {
-            Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+        // checking if sdk is higher than 30 to ask for this permission
+        if (Build.VERSION.SDK_INT >= 30) {
+            if (!Environment.isExternalStorageManager()) {
+                Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+            }
         }
 
-        var mediaPermissions = [
-            "android.permission.READ_MEDIA_IMAGES",
-            "android.permission.READ_MEDIA_VIDEO",
-            "android.permission.READ_MEDIA_AUDIO",
-            "android.permission.READ_MEDIA_VISUAL_USER_SELECTED"
-        ];
+        // media permissions.
+        if (Build.VERSION.SDK_INT >= 33) {
+            var permissions:Array<String> = [
+                "android.permission.READ_MEDIA_IMAGES",
+                "android.permission.READ_MEDIA_VIDEO",
+                "android.permission.READ_MEDIA_AUDIO"
+            ];
+            
+            // something
+            if (Build.VERSION.SDK_INT >= 34) {
+                permissions.push("android.permission.READ_MEDIA_VISUAL_USER_SELECTED");
+            }
 
-        for (permission in mediaPermissions)
-        {
-            System.requestPermission(permission);
+            Permissions.requestPermissions(permissions);
+        } else {
+            // something part 2
+            Permissions.requestPermissions(["android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"]);
         }
         #end
     }
 }
+
