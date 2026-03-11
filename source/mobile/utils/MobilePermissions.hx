9cbb6637b892e3831f9f47ad2f9dc75075faaaad
ChanceXML <joseguest1337@gmail.com>
2026-03-11 16:33:05 -0700

    will this work? idk

--- source/mobile/utils/MobilePermissions.hx
+++ source/mobile/utils/MobilePermissions.hx
@@ -2,37 +2,35 @@ package mobile.utils;
 
 import extension.androidtools.Settings;
 import extension.androidtools.os.Environment;
-import extension.androidtools.Permissions;
-import extension.androidtools.os.Build;
+import extension.androidtools.os.Permissions;
+import extension.androidtools.os.Build
+import extension.androidtools.os.Build.VERSION;       
+import extension.androidtools.os.Build.VERSION_CODES;
 
 class MobilePermissions
 {
     public static function request():Void
     {
         #if android
-        // checking if sdk is higher than 30 to ask for this permission
-        if (Build.VERSION.SDK_INT >= 30) {
+        if (VERSION.SDK_INT >= VERSION_CODES.R) {
             if (!Environment.isExternalStorageManager()) {
                 Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
             }
         }
 
-        // media permissions.
-        if (Build.VERSION.SDK_INT >= 33) {
+        if (VERSION.SDK_INT >= VERSION_CODES.TIRAMISU) {
             var permissions:Array<String> = [
                 "android.permission.READ_MEDIA_IMAGES",
                 "android.permission.READ_MEDIA_VIDEO",
                 "android.permission.READ_MEDIA_AUDIO"
             ];
             
-            // something
-            if (Build.VERSION.SDK_INT >= 34) {
+            if (VERSION.SDK_INT >= VERSION_CODES.UPSIDE_DOWN_CAKE) {
                 permissions.push("android.permission.READ_MEDIA_VISUAL_USER_SELECTED");
             }
 
             Permissions.requestPermissions(permissions);
         } else {
-            // something part 2
             Permissions.requestPermissions(["android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"]);
         }
         #end
