52fe7ea31c1312630d1f1359426139f97797e1ce
ChanceXML <joseguest1337@gmail.com>
2026-03-10 18:16:08 -0700

    dude

--- source/mobile/utils/MobilePermissions.hx
+++ source/mobile/utils/MobilePermissions.hx
@@ -2,7 +2,6 @@ package mobile.utils;
 
 #if android
 import extension.androidtools.os.Build;
-import extension.androidtools.os.Build.VERSION_CODES;
 import extension.androidtools.Permissions;
 import extension.androidtools.os.Environment;
 import extension.androidtools.Settings;
@@ -13,18 +12,16 @@ class AndroidHelper
     #if android
     public static function checkStoragePermission():Void
     {
-        if (Build.SDK_INT >= VERSION_CODES.R)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
         {
             if (!Environment.isExternalStorageManager())
-            {
                 Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
-            }
         }
         else
         {
             Permissions.requestPermissions([
-                Permissions.READ_EXTERNAL_STORAGE,
-                Permissions.WRITE_EXTERNAL_STORAGE
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE"
             ]);
         }
     }
