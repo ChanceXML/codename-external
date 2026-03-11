de002de4da31278f5cad461bcbdd0180bbce1aeb
ChanceXML <joseguest1337@gmail.com>
2026-03-10 17:51:58 -0700

    Update import.hx

--- source/mobile/import.hx
+++ source/mobile/import.hx
@@ -1,6 +1,6 @@
 #if android
 import extension.androidtools.Settings as android.Settings;
-import extension.androidtools.os.Enviroment as android.os.Enviroment;
+import extension.androidtools.os.Environment as android.os.Environment;
 import extension.androidtools.os.Build as android.os.Build;
-import extension.androidtools.Permissions as android.Permissions:
+import extension.androidtools.Permissions as android.Permissions;
 #end
