ba06d2b05abc3d7823ae5dd31075efeae9a7fbaa
ChanceXML <joseguest1337@gmail.com>
2026-03-10 18:04:25 -0700

    Update MobilePermissions.hx

--- source/mobile/utils/MobilePermissions.hx
+++ source/mobile/utils/MobilePermissions.hx
@@ -1,30 +1,24 @@
 package mobile.utils;
 
 #if android
-import AndroidBuild;
-import AndroidEnvironment;
-import AndroidSettings;
-import AndroidPermissions;
+import extension.androidtools.os.Build as AndroidBuild;
+import extension.androidtools.os.Environment as AndroidEnvironment;
+import extension.androidtools.Settings as AndroidSettings;
+import extension.androidtools.Permissions as AndroidPermissions;
 #end
 
 class AndroidHelper {
 
     public static function checkStoragePermission():Void {
         #if android
-        // Check if device is Android 11+ (API 30)
-        if (Build.VERSION.SDK_INT >= 30) {
-            
-    
-            if (!Environment.isExternalStorageManager()) {
-                // Opens Menu
-                Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+        if (AndroidBuild.SDK_INT >= 30) {
+            if (!AndroidEnvironment.isExternalStorageManager()) {
+                AndroidSettings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
             }
-            
         } else {
-            // For Android 10 and below 
-            Permissions.requestPermissions([
-                Permissions.READ_EXTERNAL_STORAGE, 
-                Permissions.WRITE_EXTERNAL_STORAGE
+            AndroidPermissions.requestPermissions([
+                "android.permission.READ_EXTERNAL_STORAGE", 
+                "android.permission.WRITE_EXTERNAL_STORAGE"
             ]);
         }
         #end
