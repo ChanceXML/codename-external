1cd98298ed1a7dcce8f2a751452ad43b622e4060
ChanceXML <joseguest1337@gmail.com>
2026-03-11 15:03:13 -0700

    Update MobilePermissions.hx

--- source/mobile/utils/MobilePermissions.hx
+++ source/mobile/utils/MobilePermissions.hx
@@ -2,6 +2,7 @@ package mobile.utils;
 
 import extension.androidtools.Settings;
 import extension.androidtools.os.Environment;
+import lime.system.System;
 
 class MobilePermissions
 {
@@ -11,6 +12,18 @@ class MobilePermissions
         if (!Environment.isExternalStorageManager())
         {
             Settings.requestSetting("MANAGE_APP_ALL_FILES_ACCESS_PERMISSION");
+        }
+
+        var mediaPermissions = [
+            "android.permission.READ_MEDIA_IMAGES",
+            "android.permission.READ_MEDIA_VIDEO",
+            "android.permission.READ_MEDIA_AUDIO",
+            "android.permission.READ_MEDIA_VISUAL_USER_SELECTED"
+        ];
+
+        for (permission in mediaPermissions)
+        {
+            System.requestPermission(permission);
         }
         #end
     }
